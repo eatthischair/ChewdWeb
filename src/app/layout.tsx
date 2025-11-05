e2da1954d1a6c846a337fa21a87c186eef764c72
eatthischair <based.god.swag.od@gmail.com>
2025-11-05 13:22:11 -0600

    deployment test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className="bg-[#0a0a0a]">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
